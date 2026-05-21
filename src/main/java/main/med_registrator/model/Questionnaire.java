0fcdd4a8747fbc68d794e3c3f5d4b21c32ebcf37
Евгений <titanozxc@gmail.com>
2026-05-22 02:46:23 +0300

    добавляем ФИО - Questionnaire.java

--- src/main/java/main/med_registrator/model/Questionnaire.java
+++ src/main/java/main/med_registrator/model/Questionnaire.java
@@ -3,17 +3,20 @@ package main.med_registrator.model;
 import java.util.List;
 
 public class Questionnaire {
+    private String fullName;  
     private int age;
     private String chronic;
     private List<Symptom> symptoms;
     private int totalScore;
 
-    public Questionnaire(int age, String chronic, List<Symptom> symptoms) {
+    public Questionnaire(String fullName, int age, String chronic, List<Symptom> symptoms) {
+        this.fullName = fullName;
         this.age = age;
         this.chronic = chronic;
         this.symptoms = symptoms;
     }
 
+    public String getFullName() { return fullName; }
     public int getAge() { return age; }
     public String getChronic() { return chronic; }
     public List<Symptom> getSymptoms() { return symptoms; }
